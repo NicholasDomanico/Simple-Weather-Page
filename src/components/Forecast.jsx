0e9c68c8cb59f0637dda3750a75fb1802735f896
Nicholas Domanico <3966361+NicholasDomanico@users.noreply.github.com>
2024-03-15 10:34:26 -0400

    Switch to styled-components, style forecast box

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,25 +1,73 @@
+import styled from "styled-components";
+
 function Day(props) {
   return (
-    <>
-      <div className="day" id="day">
-        <p className="day-header">{props.day}</p>
-        <p className="forecast-temp">Temperature: </p>
-        <div id="day1-high-low" className="high-low"></div>
-        <p>Description: </p>
-        <p id="day1-description" className="description"></p>
-        <p>Rain Chance: </p>
-        <p id="day1-rain-chance" className="rain-chance"></p>
-      </div>
-    </>
+    <DayWrapper>
+      <DayHeader>{props.day}</DayHeader>
+      <ForecastTemp>Temperature: </ForecastTemp>
+      <HighLow></HighLow>
+      <p>Description: </p>
+      <Description></Description>
+      <p>Rain Chance: </p>
+      <RainChance></RainChance>
+    </DayWrapper>
   );
 }
+
+const DayWrapper = styled.section`
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-template-rows: 25% 25% 25% 25%;
+  justify-items: center;
+  align-items: center;
+  width: 100%;
+  margin-top: 10px;
+  font-size: 1.2rem;
+  font-weight: 1000;
+  color: rgb(0, 1, 33);
+`;
+
+const DayHeader = styled.p`
+  display: flex;
+  justify-content: center;
+  color: white;
+  background-color: rgb(92, 92, 255);
+  width: 100%;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  grid-column-start: 1;
+  grid-column-end: 3;
+`;
+
+const ForecastTemp = styled.div`
+  grid-row-start: 2;
+`;
+
+const HighLow = styled.div`
+  white-space: pre;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  grid-row-start: 2;
+  grid-column-start: 2;
+`;
+
+const Description = styled.p`
+  grid-row-start: 3;
+  grid-column-start: 2;
+`;
+
+const RainChance = styled.div`
+  grid-row-start: 4;
+  grid-column-start: 2;
+`;
 
 export default function Forecast(props) {
   return (
     <>
       <Day day="Today" />
       <Day day="Tommorow" />
-      <Day />
+      <Day day="Next Day" />
     </>
   );
 }
